0723891271c8fdc2e40042c6e5c50a14584ddeb9
vals25-mg <rvalisoa3@gmail.com>
2023-04-04 17:43:23 +0300

    commit sprint2

--- Framework/src/main/java/etu2034/framework/servlet/FrontServlet.java
+++ Framework/src/main/java/etu2034/framework/servlet/FrontServlet.java
@@ -1,5 +1,6 @@
 package etu2034.framework.servlet;
 
+import etu2034.framework.Mapping;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,8 +8,19 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 public class FrontServlet extends HttpServlet {
+
+    HashMap<String, Mapping> MappingUrls;
+
+    public HashMap<String, Mapping> getMappingUrls() {
+        return MappingUrls;
+    }
+
+    public void setMappingUrls(HashMap<String, Mapping> mappingUrls) {
+        MappingUrls = mappingUrls;
+    }
 
     public void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     resp.setContentType("text/html");
